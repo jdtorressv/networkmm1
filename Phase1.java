7117d36abcbdfb5ece475ec3c4b7eeee1f7702a4
Joseph Torres <jdtorres@ucdavis.edu>
2018-02-13 01:41:12 -0800

    added Phase1.java

---
+++ Phase1.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.io.*;
+
+public class Phase1 {
+
+	public static void main(String[] args) {
+		int MAXBUFFER = 100;
+		Queue<Integer> q = new LinkedList<Integer>();
+		for (int i=0; i<10; i++)
+     			q.add(i);
+ 		System.out.println("Elements of queue-"+q);
+ 		int removedele = q.remove();
+    		System.out.println("removed element-" + removedele);
+ 
+    		System.out.println(q);
+ 
+    // To view the head of queue
+    		int head = q.peek();
+   		System.out.println("head of queue-" + head);
+ 
+    // Rest all methods of collection interface,
+    // Like size and contains can be used with this
+    // implementation.
+    		int size = q.size();
+    		System.out.println("Size of queue-" + size);
+
+	}
+
+}
